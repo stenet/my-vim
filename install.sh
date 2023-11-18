d9926b57b956de65745216228357d78e085f70a9
Stefan <stefan.heim@hotmail.com>
2023-11-19 00:01:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
   && curl -fLo ~/.vimrc https://raw.githubusercontent.com/stenet/my-vim/main/.vimrc \
+  && curl -fLo ~/.vim/stef/clipboard.sh --create-dirs https://raw.githubusercontent.com/stenet/my-vim/main/clipboard.sh \
   && vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa" \
   && echo "colorscheme gruvbox" >> ~/.vimrc \
-  && mkdir -p ~/.vim/stef \
   && echo "clipboard_id=$1" > ~/.vim/stef/.env
