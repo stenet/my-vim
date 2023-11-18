597092e2301285afee85029ce1d7e9a079ba14ab
Stefan <stefan.heim@hotmail.com>
2023-11-18 15:47:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/stenet/my-vim/main/.vimrc
-
-vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa"
-
-echo "colorscheme gruvbox" >> ~/.vimrc
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+  && curl -fLo ~/.vimrc https://raw.githubusercontent.com/stenet/my-vim/main/.vimrc \
+  && vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa" \
+  && echo "colorscheme gruvbox" >> ~/.vimrc
