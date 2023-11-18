979a4c1bef8d8ede48090a7fe2a016bd6c405a46
Stefan <stefan.heim@hotmail.com>
2023-11-18 15:34:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -fLo ~/.vimrc https://raw.githubusercontent.com/stenet/my-vim/master/.vimrc
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/stenet/my-vim/main/.vimrc
 
 vim -es -u ~/.vimrc -i NONE -c "PlugInstall" -c "qa"
