6490ebf978a4be03fc3c2b2d62384624af05c9d6
Stefan <stefan.heim@hotmail.com>
2023-11-19 00:10:00 +0100

    Update clipboard.sh

--- clipboard.sh
+++ clipboard.sh
@@ -5,11 +5,11 @@ fi
 
 if [ $1 == "u" ] 
 then
-  curl -H "Content-Type: text/plain" -X POST -s -d "$2" -o /dev/null https://clipboard.code23.at/?id=$clipboard_id
+  curl -H "Content-Type: text/plain" -X POST -s -d "$2" -o /dev/null https://clipboard.code23.at/api/$clipboard_id
 elif [ $1 == "d" ] 
 then
-  curl -sfL https://clipboard.code23.at/?id=$clipboard_id
+  curl -sfL https://clipboard.code23.at/api/$clipboard_id
 elif [ $1 == "t" ]
 then
-  echo https://clipboard.code23.at/?id=$clipboard_id
+  echo https://clipboard.code23.at/api/$clipboard_id
 fi
